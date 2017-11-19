5514689894ac3e501eeb049b5bd2b496ff6b77eb
MinHo Kim <fastz123@naver.com>
2017-11-19 21:50:54 +0900

    make printmap

---
+++ printsudoku.c
@@ -0,0 +1,15 @@
+void printmap()
+{
+	for (int i = 0; i<9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			printf("%d ", sudoku[i][j]);
+			if (j == 8)
+			{
+				printf("\n");
+			}
+		}
+	}
+	return;
+}
