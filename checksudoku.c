cd02227555c41f852cebba77e7a02b2b2b1fda15
MinHo Kim <fastz123@naver.com>
2017-11-21 10:52:46 +0900

    add checksudoku horizon

--- checksudoku.c
+++ checksudoku.c
@@ -16,4 +16,21 @@ void check_sudoku(int i, int j)
 			}
 		}
 	}
+
+
+	for (int k = 0; k < 9; k++)			//수평검사
+	{
+		if (j == k)
+		{
+			continue;
+		}
+		else
+		{
+			if (sudoku[i][j] == sudoku[i][k])
+			{
+				//failed = 1;
+				return;
+			}
+		}
+	}
 }
