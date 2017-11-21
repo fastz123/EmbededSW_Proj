f62dee24a03d15c38fe5ae39152aeea7fc5b7f2f
MinHo Kim <fastz123@naver.com>
2017-11-21 10:56:37 +0900

    divide func horizon,vertical

--- checksudoku.c
+++ checksudoku.c
@@ -16,21 +16,4 @@ void check_sudoku(int i, int j)
 			}
 		}
 	}
-
-
-	for (int k = 0; k < 9; k++)			//수평검사
-	{
-		if (j == k)
-		{
-			continue;
-		}
-		else
-		{
-			if (sudoku[i][j] == sudoku[i][k])
-			{
-				//failed = 1;
-				return;
-			}
-		}
-	}
 }
