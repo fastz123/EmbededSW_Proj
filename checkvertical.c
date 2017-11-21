4d80deddde19d68b1e19c068ce0a910eee02f87e
MinHo Kim <fastz123@naver.com>
2017-11-21 11:06:01 +0900

    make check vertical func

---
+++ checkvertical.c
@@ -0,0 +1,19 @@
+void check_vertical(int i, int j)
+{
+	//int failed = 0;
+	for (int k = 0; k < 9; k++)	//수직검사
+	{
+		if (i == k)
+		{
+			continue;
+		}
+		else
+		{
+			if (sudoku[i][j] == sudoku[k][j])
+			{
+				//failed = 1;
+				return;
+			}
+		}
+	}
+}
