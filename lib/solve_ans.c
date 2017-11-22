d0246543dde430640c68e98f12d713990f2d0009
Jinuk <ranid7777@naver.com>
2017-11-22 04:34:54 -0800

    add solve_ans.c

---
+++ lib/solve_ans.c
@@ -0,0 +1,30 @@
+void solve_ans(int i, int j)
+{
+	if(i ==8 && j ==8)
+	{
+		cnt++;
+		printmap();
+		return;
+	}
+	else
+	{
+		increment(&i, &j);
+		while (readonlySudoku[i][j]>0)
+		{
+			if (i == 8 && j == 8)
+			{
+				printmap();
+				cnt++;
+				return;
+			}
+			increment(&i, &j);
+		}
+		for (int v = 1; v <= 9; v++)
+		{
+			sudoku[i][j] = v;
+			check_sudoku(i, j);
+		}
+		sudoku[i][j] = 0;
+		return;
+	}
+}
