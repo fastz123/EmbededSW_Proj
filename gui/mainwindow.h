edd86f859ad39326f52de81e92a5ad5a55044ead
Jinuk <ranid7777@naver.com>
2017-11-23 20:20:56 -0800

    no change

--- gui/mainwindow.h
+++ gui/mainwindow.h
@@ -15,6 +15,11 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_textEdit_8_textChanged();
+
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
