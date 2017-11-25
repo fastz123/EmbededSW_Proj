86e0ee6b0cb0f2ae73dbeece76460fa11a0d9648
MinHo Kim <fastz123@naver.com>
2017-11-25 11:52:04 +0900

    modify myapp.c

--- my_sudoku/myapp.c
+++ my_sudoku/myapp.c
@@ -13,6 +13,13 @@ void main()
 	{
 		increment(&i, &j);
 	}
+
+	for (int v = 1; v <= 9; v++)
+	{
+		sudoku[i][j] = v;
+		check_sudoku(i, j);
+		//sudoku[i][j] = 0;
+	}
 	//printf("위 스도쿠는 정답이 %d개입니다.\n", cnt);
 	//printmap();
 }
