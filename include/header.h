2ecfbabe3291699f12b907b98670f8c7fb690390
MinHo Kim <fastz123@naver.com>
2017-11-26 12:43:45 +0900

    modify header file

--- include/header.h
+++ include/header.h
@@ -1,10 +1,12 @@
 #define HEAD_1
 #ifndef HEAD_1
 
-int increment;
-int sudoku_data;
-int print_sudoku;
-int check_horizon;
-int check_vertical;
+int increment();
+int sudoku_data();
+int print_sudoku();
+int check_horizon();
+int check_vertical();
+int check_3by3();
+int check_finish();
 
 #endif
